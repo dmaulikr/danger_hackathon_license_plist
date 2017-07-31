447a06cbde30faafe66dc05fa691302d31abab26
Kohei Tabata <nerd0geek1@gmail.com>
2017-07-31 19:38:28 +0900

    Initial commit

---
+++ DangerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DangerSample
+//
+//  Created by Kohei Tabata on 7/31/17.
+//  Copyright © 2017 Kohei Tabata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
